2103d29015e4ac7c143610213e6d8c7609d4637e
aikhe <ikeandrie.ro@gmail.com>
2025-10-01 03:11:36 +0800

    feat(prototype): utilize CURLOPT_WRITEDATA to write received data & more memory allocations

--- prototype.c
+++ prototype.c
@@ -1,26 +1,45 @@
 #include <curl/curl.h>
+#include <memoryapi.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
-  size_t bytes = size * nmemb;
+typedef struct Memory {
+  char *response;
+  size_t size;
+} Memory;
+
+static size_t write_callback(char *ptr, size_t size, size_t nmemb,
+                             void *userdata) {
+  size_t totalBytes = size * nmemb;
+  struct Memory *mem = (struct Memory *)userdata;
   int lineNum = 1;
 
-  printf("total chunk bytes: %zu\n", bytes);
+  char *temp = realloc(mem->response, mem->size + totalBytes + 1);
+  if (!ptr) {
+    fprintf(stderr, "[ERROR] Failed to realloc memory. \n");
+    return 0;
+  }
+
+  mem->response = temp;
+  memcpy(&(mem->response[mem->size]), ptr, totalBytes);
+  mem->size += totalBytes;
+  mem->response[mem->size] = 0;
+
+  printf("total chunk totalBytes: %zu\n", totalBytes);
   printf("%d:\t", lineNum);
-  for (int i = 0; i < bytes; i++) {
+  for (int i = 0; i < totalBytes; i++) {
     printf("%c", ptr[i]);
     if (ptr[i] == '\n') {
       lineNum++;
-      printf("%d\t", lineNum);
+      printf("%d:\t", lineNum);
     }
   }
   printf("\n\n");
   // fwrite(ptr, size, nmemb, stdout);
 
-  return bytes;
+  return totalBytes;
 }
 
 int main(void) {
@@ -36,12 +55,16 @@ int main(void) {
   CURL *curl;
   CURLcode res;
 
+  Memory mem = {malloc(1), 0};
+
   curl = curl_easy_init();
 
   printf("curl handle address: %p\n\n", curl);
   if (curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jacobsorber.com");
+    // curl_easy_setopt(curl, CURLOPT_URL, "https://jacobsorber.com");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&mem);
 
     // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
     curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert-2025-09-09.pem");
@@ -51,6 +74,8 @@ int main(void) {
     if (res != CURLE_OK) {
       fprintf(stderr, "curl_easy_perform() failed. %s\n",
               curl_easy_strerror(res));
+    } else {
+      printf("\nFrom mem->response:\n%s\n", mem.response);
     }
 
     curl_easy_cleanup(curl);
