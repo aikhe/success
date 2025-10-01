13a77528b996464efdb88a666df51047c44471b5
aikhe <ikeandrie.ro@gmail.com>
2025-10-02 01:15:35 +0800

    feat(gemini): fgets takes user prompt & parse and update request body json

--- prototype.c
+++ prototype.c
@@ -47,17 +47,16 @@ static size_t write_callback(char *ptr, size_t size, size_t nmemb,
   mem->size += total_bytes;
   mem->response[mem->size] = '\0';
 
-  printf("total chunk total_bytes: %zu\n", total_bytes);
-  printf("%d:\t", lineNum);
+  // printf("total chunk total_bytes: %zu\n", total_bytes);
+  // printf("%d:\t", lineNum);
   for (int i = 0; i < total_bytes; i++) {
-    printf("%c", ptr[i]);
+    // printf("%c", ptr[i]);
     if (ptr[i] == '\n') {
       lineNum++;
-      printf("%d:\t", lineNum);
+      // printf("%d:\t", lineNum);
     }
   }
-  printf("\n\n");
-  // fwrite(ptr, size, nmemb, stdout);
+  // printf("\n\n");
 
   return total_bytes;
 }
@@ -69,10 +68,10 @@ int main(void) {
     fprintf(stderr, "[ERROR] Operation failed. \n");
     return EXIT_FAILURE;
   }
-
   printf("[INFO] Success succeeded!\n\n");
+
   char *env_json = read_file("env.json");
-  printf("%s\n", env_json);
+  printf("[DEBUG] env.json:\n%s\n", env_json);
   cJSON *env = cJSON_Parse(env_json);
   if (!env) {
     const char *error_ptr = cJSON_GetErrorPtr();
@@ -81,7 +80,7 @@ int main(void) {
       fprintf(stderr, "[ERROR] Error parsing JSON at %s\n", error_ptr);
     }
 
-    free(env);
+    free(env_json);
     return EXIT_FAILURE;
   }
 
@@ -95,21 +94,40 @@ int main(void) {
   if (!gemini_api_url->valuestring) {
     fprintf(stderr, "GEMINI_API_URL environment variable not set.\n");
   }
-  printf("API: %s\n", gemini_api_key->valuestring);
-  printf("URL: %s\n", gemini_api_url->valuestring);
+  // printf("API: %s\n", gemini_api_key->valuestring);
+  // printf("URL: %s\n", gemini_api_url->valuestring);
 
   CURL *curl;
   CURLcode res;
 
   Memory mem = {malloc(1), 0};
 
-  char *gemini_header_json = read_file("gemini_header.json");
-  printf("%s\n", gemini_header_json);
+  char *req_body = read_file("gemini_req_body.json");
+  char userPrompt[256];
+  printf("Enter your prompt: ");
+  if (fgets(userPrompt, sizeof(userPrompt), stdin) != NULL) {
+    userPrompt[strcspn(userPrompt, "\n")] = '\0'; // remove newline
+
+    printf("User prompt: %s\n\n", userPrompt);
+  }
+  cJSON *req_body_json = cJSON_Parse(req_body);
+  cJSON *contents = cJSON_GetObjectItemCaseSensitive(req_body_json, "contents");
+  cJSON *contents_arr = cJSON_GetArrayItem(contents, 0);
+  cJSON *parts = cJSON_GetObjectItemCaseSensitive(contents_arr, "parts");
+  cJSON *parts_arr = cJSON_GetArrayItem(parts, 0);
+  cJSON *text = cJSON_GetObjectItemCaseSensitive(parts_arr, "text");
+  cJSON_SetValuestring(text, userPrompt);
+
+  char *req_body_json_str = cJSON_PrintUnformatted(req_body_json);
+  printf("Request body:\n");
+  printf("%s\n", req_body);
+  printf("Updated request body:\n");
+  printf("%s\n\n", req_body_json_str);
 
   curl_global_init(CURL_GLOBAL_DEFAULT);
   curl = curl_easy_init();
 
-  printf("\ncurl handle address: %p\n\n", curl);
+  // printf("\ncurl handle address: %p\n\n", curl);
 
   if (curl) {
     struct curl_slist *list = NULL;
@@ -119,7 +137,7 @@ int main(void) {
 
     snprintf(auth_header, sizeof(auth_header), "%s %s", header_name,
              gemini_api_key->valuestring);
-    printf("%s\n", auth_header);
+    // printf("%s\n", auth_header);
 
     list = curl_slist_append(list, auth_header);
     list = curl_slist_append(list, content_type);
@@ -129,17 +147,22 @@ int main(void) {
     curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 5L);
     curl_easy_setopt(curl, CURLOPT_TCP_NODELAY, 1L);
 
+    // verbose logging
+    // curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
     // curl_easy_setopt(curl, CURLOPT_URL, "https://jacobsorber.com");
     curl_easy_setopt(curl, CURLOPT_URL, gemini_api_url->valuestring);
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, list);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, gemini_header_json);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, req_body_json_str);
     curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE,
-                     (long)strlen(gemini_header_json));
+                     (long)strlen(req_body_json_str));
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&mem);
 
-    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-    curl_easy_setopt(curl, CURLOPT_CAINFO, "cacert-2025-09-09.pem");
+    // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // removes SSL
+    // verification
+    curl_easy_setopt(curl, CURLOPT_CAINFO,
+                     "cacert-2025-09-09.pem"); // set cacert for ssl
 
     res = curl_easy_perform(curl);
 
@@ -152,15 +175,15 @@ int main(void) {
     cJSON *first_part = cJSON_GetArrayItem(parts, 0);
     cJSON *text = cJSON_GetObjectItemCaseSensitive(first_part, "text");
 
-    printf("\nGemini response text: %s\n", text->valuestring);
+    printf("Gemini response: %s\n", text->valuestring);
     cJSON_Delete(mem_res);
 
     if (res != CURLE_OK) {
       fprintf(stderr, "curl_easy_perform() failed. %s\n",
               curl_easy_strerror(res));
     } else {
-      printf("\nmem->response address: %p\n", &mem);
-      printf("\nFrom mem->response:\n%s\n", mem.response);
+      // printf("\nmem->response address: %p\n", &mem);
+      // printf("\nFrom mem->response:\n%s\n", mem.response);
     }
 
     curl_slist_free_all(list);
@@ -171,9 +194,12 @@ int main(void) {
 
   free(env_json);
   cJSON_Delete(env);
-  free(gemini_header_json);
+  free(req_body);
   free(mem.response);
   curl_global_cleanup();
+
+  printf("\n[INFO] Program finished. Press ENTER to exit...\n");
+  getchar();
 
   return EXIT_SUCCESS;
 }
